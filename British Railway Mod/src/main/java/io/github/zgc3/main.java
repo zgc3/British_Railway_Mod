f5236d00c962b251a6b01fa6a4a8247a8eac4cae
zgc3 <zgc20090914@outlook.com>
2023-03-03 21:50:54 +0800

    Initial commit

---
+++ British Railway Mod/src/main/java/io/github/zgc3/main.java
@@ -0,0 +1,14 @@
+package io.github.zgc3;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.("british_railway_mod")
+@Mod.EventBusSubscriber
+public class main {
+
+
+    @SubscribeEvent
+    public static void
+
+}
